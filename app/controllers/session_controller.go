3b64df12441cb4d2a3f1dd23b746aff22ef8761a
hejiayu <724412707@qq.com>
2025-09-28 16:32:44 +0800

    fix: 登陆逻辑待处理

--- app/controllers/session_controller.go
+++ app/controllers/session_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"go-api/app/utils"
 	"net/http"
 
@@ -10,7 +11,19 @@ import (
 type SessionController struct {
 }
 
+type LoginRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func (c *SessionController) Login(ctx *gin.Context) {
+	var loginRequest LoginRequest
+	if err := ctx.ShouldBindJSON(&loginRequest); err != nil {
+		utils.ResponseError(ctx, gin.H{}, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	// todo 获取数据库数据进行校验等
 	token, err := utils.GenerateToken(13800);
 	if err != nil {
 		utils.ResponseError(ctx, gin.H{}, "Login failed", http.StatusInternalServerError)
