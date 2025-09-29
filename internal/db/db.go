5992a2fc956ca1ad466e7d5037e9312edb920db2
hejiayu <724412707@qq.com>
2025-09-29 15:52:53 +0800

    go-api基础搭建完成

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"go-api/internal/config"
+	"go-api/internal/model"
 	"log"
 
 	"gorm.io/driver/mysql"
@@ -12,13 +13,16 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	config := config.Get().DB
+	dbConfig := config.Get().DB
 	
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.User, config.Password, config.Host, config.Port, config.Database)
-	fmt.Println(dsn)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect database: %v", err)
 	}
 	DB = db
+
+	if (config.Get().App.Env == "development") {
+		DB.AutoMigrate(&model.User{})
+	}
 }
